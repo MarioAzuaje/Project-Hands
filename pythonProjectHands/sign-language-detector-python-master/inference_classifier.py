684fd46f2a9208f10f687254e0e139aca512b75f
marioAdev <121322751+MarioAzuaje@users.noreply.github.com>
2025-02-17 23:23:43 -0400

    Actualizacion farmacia y img directora

--- pythonProjectHands/sign-language-detector-python-master/inference_classifier.py
+++ pythonProjectHands/sign-language-detector-python-master/inference_classifier.py
@@ -18,7 +18,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E'}
+labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J'}
 while True:
 
     data_aux = []
